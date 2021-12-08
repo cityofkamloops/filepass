74bcb90378779240bc702d87b7a440aed7fe0628
Orlund <orlund@users.noreply.github.com>
2021-12-08 15:30:54 -0800

    add exception catching

--- filepass.py
+++ filepass.py
@@ -44,63 +44,26 @@ def transfer_file(from_fs, to_fs, filename):
     to_fs.writefile(filename, from_fs.open(filename, "rb"))
 
 
-def main():
-    # Load Environmental Variables
-
-    # GREYLOG_SERVER
-    # GREYLOG_PORT
-    # INTEGRATION_NAME
-
-    from_user = os.environ.get("FROMUSER")
-    from_pw = os.environ.get("FROMPW")
-    from_svr = os.environ.get("FROMSVR")
-    from_port = os.environ.get("FROMPORT")
-    from_dir = os.environ.get("FROMDIR")
-    from_share = os.environ.get("FROMSMBSHARE")
-    from_method = os.environ.get("FROMMETHOD")
-    from_delete = os.environ.get("FROMDELETE")
-    from_filter = os.environ.get("FROMFILEFILTER")
-
-    to_user = os.environ.get("TOUSER")
-    to_pw = os.environ.get("TOPW")
-    to_svr = os.environ.get("TOSVR")
-    to_port = os.environ.get("TOPORT")
-    to_dir = os.environ.get("TODIR")
-    to_share = os.environ.get("TOSMBSHARE")
-    to_method = os.environ.get("TOMETHOD")
-    to_delete = os.environ.get("TODELETE")
-
-    filepass_logger = logging.getLogger("filepass_logger")
-    filepass_logger.setLevel(logging.DEBUG)
-
-    handler = graypy.GELFTCPHandler(
-        os.environ.get("GREYLOG_SERVER"), int(os.environ.get("GREYLOG_PORT"))
-    )
-    filepass_logger.addHandler(handler)
-
-    handler_std = logging.StreamHandler(sys.stdout)
-    filepass_logger.addHandler(handler_std)
-
-    logger = logging.LoggerAdapter(
-        filepass_logger,
-        {
-            "from_method": from_method,
-            "from_user": from_user,
-            "from_svr": from_svr,
-            "from_port": from_port,
-            "from_share": from_share,
-            "from_dir": from_dir,
-            "from_filter": from_filter,
-            "to_method": to_method,
-            "to_user": to_user,
-            "to_svr": to_svr,
-            "to_port": to_port,
-            "to_share": to_share,
-            "to_dir": to_dir,
-            "integration": "filepass",
-            "filepass_name": os.environ.get("INTEGRATION_NAME"),
-        },
-    )
+def file_pass(
+    logger,
+    from_user,
+    from_pw,
+    from_svr,
+    from_port,
+    from_dir,
+    from_share,
+    from_method,
+    from_delete,
+    from_filter,
+    to_user,
+    to_pw,
+    to_svr,
+    to_port,
+    to_dir,
+    to_share,
+    to_method,
+    to_delete,
+):
 
     # From File System
     if from_method == "ssh":
@@ -170,6 +133,89 @@ def main():
 
     from_fs.close()
     to_fs.close()
+
+
+def main():
+
+    # Load Environmental Variables
+
+    # GREYLOG_SERVER
+    # GREYLOG_PORT
+    # INTEGRATION_NAME
+
+    from_user = os.environ.get("FROMUSER")
+    from_pw = os.environ.get("FROMPW")
+    from_svr = os.environ.get("FROMSVR")
+    from_port = os.environ.get("FROMPORT")
+    from_dir = os.environ.get("FROMDIR")
+    from_share = os.environ.get("FROMSMBSHARE")
+    from_method = os.environ.get("FROMMETHOD")
+    from_delete = os.environ.get("FROMDELETE")
+    from_filter = os.environ.get("FROMFILEFILTER")
+
+    to_user = os.environ.get("TOUSER")
+    to_pw = os.environ.get("TOPW")
+    to_svr = os.environ.get("TOSVR")
+    to_port = os.environ.get("TOPORT")
+    to_dir = os.environ.get("TODIR")
+    to_share = os.environ.get("TOSMBSHARE")
+    to_method = os.environ.get("TOMETHOD")
+    to_delete = os.environ.get("TODELETE")
+
+    filepass_logger = logging.getLogger("filepass_logger")
+    filepass_logger.setLevel(logging.DEBUG)
+
+    handler = graypy.GELFTCPHandler(
+        os.environ.get("GREYLOG_SERVER"), int(os.environ.get("GREYLOG_PORT"))
+    )
+    filepass_logger.addHandler(handler)
+
+    handler_std = logging.StreamHandler(sys.stdout)
+    filepass_logger.addHandler(handler_std)
+
+    logger = logging.LoggerAdapter(
+        filepass_logger,
+        {
+            "from_method": from_method,
+            "from_user": from_user,
+            "from_svr": from_svr,
+            "from_port": from_port,
+            "from_share": from_share,
+            "from_dir": from_dir,
+            "from_filter": from_filter,
+            "to_method": to_method,
+            "to_user": to_user,
+            "to_svr": to_svr,
+            "to_port": to_port,
+            "to_share": to_share,
+            "to_dir": to_dir,
+            "integration": "filepass",
+            "filepass_name": os.environ.get("INTEGRATION_NAME"),
+        },
+    )
+    try:
+        file_pass(
+            logger,
+            from_user,
+            from_pw,
+            from_svr,
+            from_port,
+            from_dir,
+            from_share,
+            from_method,
+            from_delete,
+            from_filter,
+            to_user,
+            to_pw,
+            to_svr,
+            to_port,
+            to_dir,
+            to_share,
+            to_method,
+            to_delete,
+        )
+    except:
+        logger.exception("Critical error found", stack_info=True)
 
 
 if __name__ == "__main__":
